d15dc0dd1119fc87a7767404b75815b8fb6f30db
Andrew Lazenka <andrewlazenka@gmail.com>
2021-08-30 01:13:37 -0400

    Move .zshrc into home dir

--- setup-zsh.sh
+++ setup-zsh.sh
@@ -2,7 +2,8 @@
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # move zsh profile into home directory
-# rename user within file
+rm ~/.zshrc
+ln -s .zshrc ~/.zshrc
 
 git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
 ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
