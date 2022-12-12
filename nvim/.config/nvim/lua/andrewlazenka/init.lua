960f29581280691def3f2ad20778a9623a561664
Andrew Lazenka <andrewlazenka@gmail.com>
2022-12-12 10:17:43 -0500

    nvim install & config

---
+++ nvim/.config/nvim/lua/andrewlazenka/init.lua
@@ -0,0 +1,3 @@
+require('andrewlazenka.keymap')
+require('andrewlazenka.set')
+require('andrewlazenka.packer')
---
+++ nvim/.config/nvim/lua/andrewlazenka/set.lua
@@ -0,0 +1,20 @@
+-- Options
+
+vim.opt.clipboard = 'unnamedplus' -- Enables the clipboard between Vim/Neovim and other applications.
+vim.opt.completeopt = 'noinsert,menuone,noselect' -- Modifies the auto-complete menu to behave more like an IDE.
+vim.opt.cursorline = true -- Highlights the current line in the editor
+vim.opt.hidden = true -- Hide unused buffers
+vim.opt.autoindent = true -- Indent a new line
+vim.opt.inccommand = 'split' -- Show replacements in a split screen
+vim.opt.guicursor = '' -- Set cursor as block
+vim.opt.number = true -- Shows the line number on the current line
+vim.opt.relativenumber = true -- Shows line number relative to the cursor
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.title = true -- Show file title
+vim.opt.wildmenu = true -- Show a more advance menu
+vim.opt.colorcolumn = '80' -- Show at 80 column a border for good code style
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.scrolloff = 8 -- start scrolling view N lines from bottom of the screen
+vim.opt.ttyfast = true -- Speed up scrolling in Vim
