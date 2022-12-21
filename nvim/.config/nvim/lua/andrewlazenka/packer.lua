7a3535ce76ffa23f3685bc89000953934e19be14
Andrew Lazenka <andrewlazenka@gmail.com>
2022-12-21 16:07:46 -0500

    Install new auto-complete & LSP

--- nvim/.config/nvim/lua/andrewlazenka/packer.lua
+++ nvim/.config/nvim/lua/andrewlazenka/packer.lua
@@ -1,12 +1,16 @@
+vim.cmd.packadd('packer.nvim')
+
 return require("packer").startup(function(use)	
+		-- packer can manage itself
 		use 'wbthomason/packer.nvim'
+
 		use 'nvim-lualine/lualine.nvim' -- Status bar & tabline
 		use 'ryanoasis/vim-devicons' -- DevIcons (for NerdTree & airline)
 		use 'tpope/vim-commentary' -- Quick comment tools
-		use 'neoclide/coc.nvim'  -- Auto Completion
 		use 'nvim-lua/plenary.nvim'
 		use {
 				'nvim-telescope/telescope.nvim', 
+				requires = { {'nvim-lua/plenary.nvim'} },
 				tag = '0.1.0'
 		}
 		use 'ThePrimeagen/harpoon'
@@ -19,8 +23,28 @@ return require("packer").startup(function(use)
 		use('tpope/vim-fugitive')
 		use {
 				'nvim-tree/nvim-tree.lua',
+				requires = { {'nvim-tree/nvim-web-devicons'} }
+		}
+
+		  use {
+				'VonHeikemen/lsp-zero.nvim',
 				requires = {
-						'nvim-tree/nvim-web-devicons',
+						-- LSP Support
+						{'neovim/nvim-lspconfig'},
+						{'williamboman/mason.nvim'},
+						{'williamboman/mason-lspconfig.nvim'},
+
+						-- Autocompletion
+						{'hrsh7th/nvim-cmp'},
+						{'hrsh7th/cmp-buffer'},
+						{'hrsh7th/cmp-path'},
+						{'saadparwaiz1/cmp_luasnip'},
+						{'hrsh7th/cmp-nvim-lsp'},
+						{'hrsh7th/cmp-nvim-lua'},
+
+						-- Snippets
+						{'L3MON4D3/LuaSnip'},
+						{'rafamadriz/friendly-snippets'},
 				}
 		}
 end)
