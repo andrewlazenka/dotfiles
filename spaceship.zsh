17cc5368802baea902df8e332f9d48e380211062
Andrew Lazenka <andrewlazenka@gmail.com>
2022-12-22 11:25:37 -0500

    Update zsh & plugins install

--- spaceship.zsh
+++ spaceship.zsh
@@ -1,12 +1,12 @@
 SPACESHIP_PROMPT_ORDER=(
 	time
-  user          # Username section
-  host          # Hostname section
-  dir           # Current directory section
-  git           # Git section (git_branch + git_status)
-  hg            # Mercurial section (hg_branch  + hg_status)
-  jobs          # Background jobs indicator
-  exit_code     # Exit code section
+	user          # Username section
+	host          # Hostname section
+	dir           # Current directory section
+	git           # Git section (git_branch + git_status)
+	hg            # Mercurial section (hg_branch  + hg_status)
+	jobs          # Background jobs indicator
+	exit_code     # Exit code section
 )
 SPACESHIP_PROMPT_ADD_NEWLINE=false
 SPACESHIP_GIT_SYMBOL='\0'
