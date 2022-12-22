b32e5fdb565e0880481459c8f9c4b5fc86188b14
Andrew Lazenka <andrewlazenka@gmail.com>
2022-12-22 08:58:54 -0500

    File styling tweaks

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -3,9 +3,9 @@ local lsp = require("lsp-zero")
 lsp.preset("recommended")
 
 lsp.ensure_installed({
-		"tsserver",
-		"eslint",
-		"sumneko_lua",
+	"tsserver",
+	"eslint",
+	"sumneko_lua",
 })
 
 lsp.nvim_workspace()
