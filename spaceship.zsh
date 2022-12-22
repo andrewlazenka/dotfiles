a66de92f4af514cd24eeefe0ce7a4af49d2f949c
Andrew Lazenka <andrewlazenka@gmail.com>
2022-12-22 11:21:10 -0500

    Add spaceship config

---
+++ spaceship.zsh
@@ -0,0 +1,14 @@
+SPACESHIP_PROMPT_ORDER=(
+	time
+  user          # Username section
+  host          # Hostname section
+  dir           # Current directory section
+  git           # Git section (git_branch + git_status)
+  hg            # Mercurial section (hg_branch  + hg_status)
+  jobs          # Background jobs indicator
+  exit_code     # Exit code section
+)
+SPACESHIP_PROMPT_ADD_NEWLINE=false
+SPACESHIP_GIT_SYMBOL='\0'
+SPACESHIP_TIME_SHOW=true
+
