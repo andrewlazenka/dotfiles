58106fa1b0f152697e51e8165abd6ec8e99fe366
Andrew Lazenka <andrewlazenka@github.com>
2023-01-06 15:02:05 -0500

    Add remaps for page up & down

--- nvim/.config/nvim/lua/andrewlazenka/keymap.lua
+++ nvim/.config/nvim/lua/andrewlazenka/keymap.lua
@@ -1,3 +1,7 @@
 -- nvim-tree
 vim.keymap.set("n", "<C-t>", ":NvimTreeToggle<CR>")
 vim.keymap.set("n", "<C-f>", ":NvimTreeFocus<CR>")
+
+-- core
+vim.keymap.set("n", "<C-j>", "<PageDown>")
+vim.keymap.set("n", "<C-k>", "<PageUp>")
