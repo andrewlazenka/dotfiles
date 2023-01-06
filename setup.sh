ee410faf9652ad7e30f4079a4a4cf4fb387b34eb
Andrew Lazenka <andrewlazenka@github.com>
2023-01-06 14:53:39 -0500

    Symlink zshrc into home dir for codespaces

--- setup.sh
+++ setup.sh
@@ -2,9 +2,18 @@
 
 # Codespaces setup script
 
+DOTFILES_DIR = /workspaces/.codespaces/.persistedshare/dotfiles
+
+# nvim
+
 # Symlink nvim config
 mkdir -p ~/.config
-ln -s /workspaces/.codespaces/.persistedshare/dotfiles/nvim/.config/nvim ~/.config/nvim 
+ln -s $DOTFILES_DIR/nvim/.config/nvim ~/.config/nvim 
 
 # Auto install packer plugins to avoid yelling errors on first boot
 nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
+
+# zsh
+
+ln -s $DOTFILES_DIR/.zshrc ~/.zshrc 
+ln -s $DOTFILES_DIR/.aliases ~/.aliases 
