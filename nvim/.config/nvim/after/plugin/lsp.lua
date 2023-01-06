3f16aa377e7040eea543a950da46ff211d20df38
Andrew Lazenka <andrewlazenka@github.com>
2023-01-06 12:16:26 -0500

    Additional default language servers

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -6,11 +6,13 @@ lsp.ensure_installed({
 	"tsserver",
 	"eslint",
 	"sumneko_lua",
+	"ruby-lsp",
+	"bash-lsp"
 })
 
 lsp.nvim_workspace()
 
-lsp.on_attach(function(client, bufnr)
+lsp.on_attach(function(_, bufnr)
 	local opts = { buffer = bufnr, remap = false }
 
 	vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
