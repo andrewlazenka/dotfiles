78b3f36c02d89afab792147a54e70a6360e14b68
Andrew Lazenka <andrewlazenka@github.com>
2023-01-06 12:17:03 -0500

    Add auto-install for Codespaces, nvim packages

--- setup.sh
+++ setup.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 
+# Codespaces setup script
+
+# Symlink nvim config
 mkdir -p ~/.config
 ln -s /workspaces/.codespaces/.persistedshare/dotfiles/nvim/.config/nvim ~/.config/nvim 
+
+# Auto install packer plugins to avoid yelling errors on first boot
+nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
