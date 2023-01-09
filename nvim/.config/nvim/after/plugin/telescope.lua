27efbb20a8ebeee30f82bb314cd7bdadc265a8be
Andrew Lazenka <andrewlazenka@gmail.com>
2023-01-09 09:45:51 -0500

    Telescope search dotfiles & ignore git

--- nvim/.config/nvim/after/plugin/telescope.lua
+++ nvim/.config/nvim/after/plugin/telescope.lua
@@ -24,6 +24,12 @@ telescope.setup {
 	pickers = {
 		find_files = {
 			hidden = true
+		},
+		live_grep = {
+			additional_args = function()
+				return { "--hidden" }
+			end,
+			glob_pattern = '!.git'
 		}
 	}
 }
