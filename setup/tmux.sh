ffab282748fc114b85154185d273f134b3084c53
Andrew Lazenka <andrewlazenka@gmail.com>
2023-03-15 22:54:44 -0400

    Add onedarker design & mem-cpu plugin

--- setup/tmux.sh
+++ setup/tmux.sh
@@ -3,3 +3,6 @@
 # move tmux config into home directory
 rm ~/.tmux.conf
 ln -s ~/Code/andrewlazenka/dotfiles/.tmux.conf ~/.tmux.conf
+
+# install tpm for plugins
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
