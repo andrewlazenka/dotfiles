ee697f4e681f35550896ef5f9d3f7527bb53ea2d
Andrew Lazenka <andrewlazenka@gmail.com>
2023-03-15 23:02:58 -0400

    Install tmux plugins from command line

--- setup/tmux.sh
+++ setup/tmux.sh
@@ -6,3 +6,6 @@ ln -s ~/Code/andrewlazenka/dotfiles/.tmux.conf ~/.tmux.conf
 
 # install tpm for plugins
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+# install plugins
+~/.tmux/plugins/tpm/scripts/install_plugins.sh
