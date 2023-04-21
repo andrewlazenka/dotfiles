c19af30644bcca437d9da96cc65f896b66763c79
Andrew Lazenka <andrewlazenka@gmail.com>
2023-04-21 12:00:40 -0400

    Add quickfix remaps

--- nvim/.config/nvim/lua/andrewlazenka/keymap.lua
+++ nvim/.config/nvim/lua/andrewlazenka/keymap.lua
@@ -5,3 +5,9 @@ vim.keymap.set("n", "<C-f>", ":NvimTreeFocus<CR>")
 -- core
 vim.keymap.set("n", "<C-j>", "<PageDown>")
 vim.keymap.set("n", "<C-k>", "<PageUp>")
+
+-- quickfix
+vim.keymap.set("n", "<leader>qo", ":copen<CR>")
+vim.keymap.set("n", "<leader>qc", ":cclose<CR>")
+vim.keymap.set("n", "<leader>qn", ":cnext<CR>")
+vim.keymap.set("n", "<leader>qp", ":cprev<CR>")
