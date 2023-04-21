6ba35fd7e21a939d0116626e3c1f2ea724b71fcc
Andrew Lazenka <andrewlazenka@gmail.com>
2023-04-21 12:00:26 -0400

    Clean up Telescope config, enable search in current buffer

--- nvim/.config/nvim/after/plugin/telescope.lua
+++ nvim/.config/nvim/after/plugin/telescope.lua
@@ -3,23 +3,13 @@ local builtin = require("telescope.builtin")
 
 vim.keymap.set("n", "<leader>sf", builtin.find_files, {})
 vim.keymap.set("n", "<C-p>", builtin.git_files, {})
--- vim.keymap.set("n", "<leader>ss", function()
--- 	builtin.grep_string({ search = vim.fn.input("Grep > ") })
--- end)
-vim.keymap.set("n", "<leader>sc", function()
-  -- You can pass additional configuration to telescope to change theme, layout, etc.
-  builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown {
-    winblend = 10,
-    previewer = false,
-  })
-end, { desc = "[/] Fuzzily search in current buffer]" })
-vim.keymap.set("n", "<leader>stf", builtin.current_buffer_fuzzy_find, { desc = "[S]earch [T]his [F]ile (current buffer)" })
-vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
-vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
-vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
-vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
-vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
-vim.keymap.set("n", "<leader>smap", builtin.keymaps, { desc = "[S]earch Normal Mode [Map]pings" })
+vim.keymap.set("n", "<leader>sb", builtin.current_buffer_fuzzy_find)
+vim.keymap.set("n", "<leader>sf", builtin.find_files)
+vim.keymap.set("n", "<leader>sh", builtin.help_tags)
+vim.keymap.set("n", "<leader>sw", builtin.grep_string)
+vim.keymap.set("n", "<leader>sg", builtin.live_grep)
+vim.keymap.set("n", "<leader>sd", builtin.diagnostics)
+vim.keymap.set("n", "<leader>smap", builtin.keymaps)
 
 telescope.setup {
 	pickers = {
@@ -31,6 +21,9 @@ telescope.setup {
 				return { "--hidden" }
 			end,
 			glob_pattern = '!.git'
-		}
+		},
+		current_buffer_fuzzy_find = {
+			sorting_strategy = 'ascending'
+		},
 	}
 }
