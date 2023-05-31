ce8e659125199c8e262c0e3c9c48a6f12d14bb65
Andrew Lazenka <andrewlazenka@gmail.com>
2023-05-31 10:28:50 -0400

    Setup Alpha

---
+++ nvim/.config/nvim/after/plugin/alpha-nvim.lua
@@ -0,0 +1,36 @@
+local fortune = require("alpha.fortune")
+
+local status_ok, alpha = pcall(require, "alpha")
+if not status_ok then
+ return
+end
+
+local dashboard = require("alpha.themes.dashboard")
+
+dashboard.section.header.val = {
+  [[                                                                   ]],
+  [[      ████ ██████           █████      ██                    ]],
+  [[     ███████████             █████                            ]],
+  [[     █████████ ███████████████████ ███   ███████████  ]],
+  [[    █████████  ███    █████████████ █████ ██████████████  ]],
+  [[   █████████ ██████████ █████████ █████ █████ ████ █████  ]],
+  [[ ███████████ ███    ███ █████████ █████ █████ ████ █████ ]],
+  [[██████  █████████████████████ ████ █████ █████ ████ ██████]],
+}
+
+dashboard.section.buttons.val = {
+   dashboard.button("f", "  Find file", ":Telescope git_files <CR>"),
+   dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
+   dashboard.button("r", "  Recently used files", ":Telescope oldfiles <CR>"),
+   dashboard.button("q", "  Quit Neovim", ":qa<CR>"),
+}
+
+dashboard.section.footer.val = fortune()
+
+dashboard.section.footer.opts.hl = "Type"
+dashboard.section.header.opts.hl = "Include"
+dashboard.section.buttons.opts.hl = "Keyword"
+
+dashboard.opts.opts.noautocmd = true
+
+alpha.setup(dashboard.opts)
