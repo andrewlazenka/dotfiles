e4aec929dcaf5e63dee6a059224764b53d7f4122
Andrew Lazenka <andrewlazenka@gmail.com>
2023-09-11 10:42:50 -0400

    Nvim move line keymap

--- nvim/.config/nvim/lua/andrewlazenka/keymap.lua
+++ nvim/.config/nvim/lua/andrewlazenka/keymap.lua
@@ -11,3 +11,7 @@ vim.keymap.set("n", "<leader>qo", ":copen<CR>")
 vim.keymap.set("n", "<leader>qc", ":cclose<CR>")
 vim.keymap.set("n", "<leader>qn", ":cnext<CR>")
 vim.keymap.set("n", "<leader>qp", ":cprev<CR>")
+
+-- move line under cursor (visual mode)
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
