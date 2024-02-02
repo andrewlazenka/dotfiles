80cf9999ae982b823a036b622709b4e8da8f454e
Andrew Lazenka <andrewlazenka@gmail.com>
2024-02-02 11:48:43 -0500

    Clean up brew install script

--- setup/brew.sh
+++ setup/brew.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+####################
+# Install Homebrew #
+####################
+#
 if ! command -v brew > /dev/null; then
     printf "[dotfiles] Install Homebrew\n"
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
@@ -8,45 +12,51 @@ fi
 brew update
 brew upgrade
 
-# save Homebrew’s installed location.
+# Save Homebrew’s installed location
+
 BREW_PREFIX=$(brew --prefix)
 
-# install GNU core utilities (those that come with macOS are outdated).
-# don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
+#################################
+# Update pre-installed programs #
+################################# 
+
 brew install coreutils
 ln -s "${BREW_PREFIX}/bin/gsha256sum" "${BREW_PREFIX}/bin/sha256sum"
-
-# install some other useful utilities like `sponge`.
 brew install moreutils
-# install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
 brew install findutils
-# install GNU `sed`, overwriting the built-in `sed`.
 brew install gnu-sed
-# install a modern version of Bash.
 brew install bash
 brew install bash-completion2
 
-# switch to using brew-installed bash as default shell
+# Homebrew-installed bash as default shell 
+
 if ! fgrep -q "${BREW_PREFIX}/bin/bash" /etc/shells; then
     echo "${BREW_PREFIX}/bin/bash" | sudo tee -a /etc/shells;
     chsh -s "${BREW_PREFIX}/bin/bash";
 fi;
 
-# install system tools
+################
+# System tools #
+################
+
 brew install vim --with-override-system-vi
 brew install nvim
 brew install grep
 brew install openssh
 brew install screen
-brew install php
 brew install gmp
 brew install git
 brew install git-lfs
 brew install tree
 brew install gcc
 brew install pnpm
+brew install cask docker
+brew install docker-compose
 
-# install fonts
+#########
+# Fonts #
+#########
+
 brew tap bramstein/webfonttools
 brew tap homebrew/cask-fonts
 
@@ -61,45 +71,58 @@ brew install font-commit-mono
 brew install font-hubot-sans
 brew install font-hack-nerd-font
 
-# install browsers
+################
+# Applications #
+################
+
+# Development
+
+brew install cask homebrew/cask-versions/visual-studio-code-insiders
+brew install cask iterm2
+brew install cask tableplus
+brew install cask postman
+brew install cask graphiql
+brew install cask rectangle
+brew install cask mac-tex
+brew install 1password
+
+# Browsers
+
 brew install cask google-chrome
 brew install cask firefox
 brew install cask brave-browser
 
-# install productivity apps
+# Communication
+
+brew install cask slack
+brew install cask discord
+brew install cask zoomus
+brew install cask loom
+brew install cask gifox
+
+# Productivity
+
 brew install cask google-drive
 brew install cask notion
 brew install cask obsidian
 brew install cask spotify
 brew install cask numi
 brew install cask calibre
-brew install cron
 
-# install communication apps
-brew install cask slack
-brew install cask discord
-brew install cask zoomus
-brew install cask loom
-
-# install development apps
-brew install cask homebrew/cask-versions/visual-studio-code-insiders
-brew install cask docker
-brew install cask iterm2
-brew install cask tableplus
-brew install cask postman
-brew install cask gifox
-brew install cask graphiql
-brew install cask rectangle
-brew install cask mac-tex
-brew install cask tmux
-brew install 1password
-brew install 1password-cli
+#############
+# Languages #
+#############
 
 brew install golang
 brew install rustup
-brew install docker-compose
+brew install php
 brew install asdf
 
+################
+# CLI programs #
+################
+
+brew install cask tmux
 brew install zsh
 brew install exa
 brew install bat
@@ -108,7 +131,6 @@ brew install duf
 brew install gh
 brew install antibody
 brew install procs
-brew install dust
 brew install btop
 brew install bottom
 brew install tealdeer
@@ -124,18 +146,29 @@ brew install lazygit
 brew install lazydocker
 brew install cava
 brew install --cask background-music
-brew install dust
+brew install ncdu
+brew install 1password-cli
 
-# ascii art
+#############
+# ASCII art #
+#############
+
 brew install pipes-sh
 brew tap sontek/snowmachine
 brew install snowmachine
 
-# install zsh plugins
+###############
+# ZSH plugins #
+###############
+
 brew install starship
 brew install zsh-autosuggestions
 brew install zsh-syntax-highlighting
 brew install zsh-completions
 brew install zsh-history-substring-search
+
+########################
+# Post-install cleanup #
+########################
 
 brew cleanup
