89b4e8babfb7a505bc132fe893eef6e7704b92d5
Andrew Lazenka <andrewlazenka@gmail.com>
2024-07-03 13:38:29 -0400

    Update dock

--- setup/dock.sh
+++ setup/dock.sh
@@ -6,10 +6,10 @@ dockutil --remove all
 # add applications
 dockutil --add "/Applications/Firefox.app"
 dockutil --add "/Applications/Spotify.app"
-dockutil --add "/Applications/Cron.app"
-dockutil --add "/Applications/Notion.app"
+dockutil --add "/Applications/Rise.app"
 dockutil --add "/Applications/Obsidian.app"
 dockutil --add "/Applications/Numi.app"
+dockutil --add "/Applications/Ghostty.app"
 dockutil --add "/Applications/iTerm.app"
 
 killall Dock
