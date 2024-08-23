f64c8eb29b7382222774290654e9527610f16bff
Andrew Lazenka <andrewlazenka@gmail.com>
2024-08-23 11:38:40 -0400

    Fix broken quickfix keymaps

--- .config/nvim/lua/andrewlazenka/keymap.lua
+++ .config/nvim/lua/andrewlazenka/keymap.lua
@@ -7,10 +7,10 @@ vim.keymap.set("n", "<C-j>", "<PageDown>")
 vim.keymap.set("n", "<C-k>", "<PageUp>")
 
 -- quickfix
-vim.keymap.set("n", "<leader>qo", ":copen<CR>")
-vim.keymap.set("n", "<leader>qc", ":cclose<CR>")
-vim.keymap.set("n", "<leader>qn", ":cnext<CR>")
-vim.keymap.set("n", "<leader>qp", ":cprev<CR>")
+-- vim.keymap.set("n", "<leader>qo", ":copen<CR>")
+-- vim.keymap.set("n", "<leader>qc", ":cclose<CR>")
+-- vim.keymap.set("n", "<leader>qn", ":cnext<CR>")
+-- vim.keymap.set("n", "<leader>qp", ":cprev<CR>")
 
 -- move line under cursor (visual mode)
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
