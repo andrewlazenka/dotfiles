2585c9e13cdad4b6e422295c15ff24bb865391e4
Andrew Lazenka <andrewlazenka@Andrews-MacBook-Pro.local>
2025-04-14 09:10:53 -0400

    Update dock

--- setup/dock.sh
+++ setup/dock.sh
@@ -4,12 +4,11 @@
 dockutil --remove all
 
 # add applications
-dockutil --add "/Applications/Firefox.app"
-dockutil --add "/Applications/Spotify.app"
-dockutil --add "/Applications/Rise.app"
+dockutil --add "/Applications/Zen.app"
 dockutil --add "/Applications/Obsidian.app"
-dockutil --add "/Applications/Numi.app"
+dockutil --add "/Applications/Cursor.app"
+dockutil --add "/Applications/Windsurf.app"
 dockutil --add "/Applications/Ghostty.app"
-dockutil --add "/Applications/iTerm.app"
+dockutil --add "/Applications/Linear.app"
 
 killall Dock
