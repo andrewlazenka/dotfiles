23437a6cbf2d30f70e0d1aacf172bae2baecc4dd
Andrew Lazenka <andrewlazenka@gmail.com>
2025-04-14 10:49:57 -0400

    Update nvim for 0.11 (not fully functional)

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,14 +1,4 @@
 return {
-	-- color schemes
-	"folke/tokyonight.nvim",
-	"lunarvim/onedarker.nvim",
-	"haishanh/night-owl.vim",
-	"macguirerintoul/night_owl_light.vim",
-	"arcticicestudio/nord-vim",
-	"rmehri01/onenord.nvim",
-	"navarasu/onedark.nvim",
-	{ "catppuccin/nvim", as = "catppuccin" },
-
 	{
 		'goolord/alpha-nvim',
 		requires = { 'nvim-tree/nvim-web-devicons' },
@@ -38,32 +28,11 @@ return {
 	},
 	"tpope/vim-fugitive",
 	"tpope/vim-abolish",
-	{
-		"nvim-tree/nvim-tree.lua",
-		dependencies = { {"nvim-tree/nvim-web-devicons"} }
-	},
 	{ "sindrets/diffview.nvim", dependencies = "nvim-lua/plenary.nvim" },
-	{
-		"VonHeikemen/lsp-zero.nvim",
-		dependencies = {
-			-- LSP Support
-			{"neovim/nvim-lspconfig"},
-			{"williamboman/mason.nvim"},
-			{"williamboman/mason-lspconfig.nvim"},
 
-			-- Autocompletion
-			{"hrsh7th/nvim-cmp"},
-			{"hrsh7th/cmp-buffer"},
-			{"hrsh7th/cmp-path"},
-			{"saadparwaiz1/cmp_luasnip"},
-			{"hrsh7th/cmp-nvim-lsp"},
-			{"hrsh7th/cmp-nvim-lua"},
-
-			-- Snippets
-			{"L3MON4D3/LuaSnip"},
-			{"rafamadriz/friendly-snippets"},
-		}
-	},
+	{"williamboman/mason.nvim"},
+	{"williamboman/mason-lspconfig.nvim"},
+	"neovim/nvim-lspconfig",
 	{
 		"folke/todo-comments.nvim",
 		dependencies = { { "nvim-lua/plenary.nvim" } },
@@ -84,8 +53,6 @@ return {
 			require("gitsigns").setup()
 		end
 	},
-	"neovim/nvim-lspconfig",
-	"jose-elias-alvarez/null-ls.nvim",
 	"MunifTanjim/prettier.nvim",
 	"simrat39/symbols-outline.nvim",
 	{
@@ -96,5 +63,15 @@ return {
 	{
 	  'stevearc/oil.nvim',
 	  dependencies = { "nvim-tree/nvim-web-devicons" },
-	}
+	},
+
+	-- color schemes
+	"folke/tokyonight.nvim",
+	"lunarvim/onedarker.nvim",
+	"haishanh/night-owl.vim",
+	"macguirerintoul/night_owl_light.vim",
+	"arcticicestudio/nord-vim",
+	"rmehri01/onenord.nvim",
+	"navarasu/onedark.nvim",
+	{ "catppuccin/nvim", as = "catppuccin" },
 }
