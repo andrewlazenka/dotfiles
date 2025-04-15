87516855568f56f8ef1025b0bd539a8bf0f6d543
Andrew Lazenka <andrewlazenka@gmail.com>
2025-04-14 23:15:20 -0400

    Update nvim configs

--- .config/nvim/after/plugin/mason.lua
+++ .config/nvim/after/plugin/mason.lua
@@ -1,5 +1,24 @@
+local lspconfig = require("lspconfig")
+local mason_lspconfig = require("mason-lspconfig")
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+local on_attach = function(client, bufnr)
+    vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+    local opts = { noremap=true, silent=true, buffer=bufnr }
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+    vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+    vim.keymap.set("n", "vd", vim.diagnostic.open_float, opts)
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+
+    if client.server_capabilities.documentSymbolProvider then
+        require("nvim-navic").attach(client, bufnr)
+    end
+end
+
 require("mason").setup()
-require("mason-lspconfig").setup {
+mason_lspconfig.setup({
     ensure_installed = {
         "astro",
         "bashls",
@@ -15,10 +34,12 @@ require("mason-lspconfig").setup {
         "tailwindcss",
         "ts_ls",
     },
-}
-
-require("mason-lspconfig").setup_handlers {
-	function (server_name)
-		require("lspconfig")[server_name].setup {}
-	end
-}
+    handlers = {
+        function (server_name)
+            require("lspconfig")[server_name].setup {
+                on_attach = on_attach,
+                capabilities = capabilities,
+            }
+        end
+    }
+})
--- .config/nvim/after/plugin/prettier.lua
+++ .config/nvim/after/plugin/prettier.lua
@@ -3,6 +3,7 @@ local prettier = require("prettier")
 prettier.setup({
   bin = "prettier",
   filetypes = {
+    "astro",
     "css",
     "graphql",
     "html",
