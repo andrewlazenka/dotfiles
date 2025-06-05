439689602666fa73e593196bf675d49dd03d06b2
Andrew Lazenka <andrewlazenka@gmail.com>
2025-06-05 11:41:56 -0400

    Overhaul nvim config, mostly Lazy

---
+++ .config/nvim/lua/plugins/colorschemes.lua
@@ -0,0 +1,13 @@
+return {
+	"folke/tokyonight.nvim",
+	"lunarvim/onedarker.nvim",
+	"haishanh/night-owl.vim",
+	"macguirerintoul/night_owl_light.vim",
+	"arcticicestudio/nord-vim",
+	"rmehri01/onenord.nvim",
+	"navarasu/onedark.nvim",
+	{ "catppuccin/nvim", as = "catppuccin" },
+	"neanias/everforest-nvim",
+	{ "EdenEast/nightfox.nvim" },
+	"rebelot/kanagawa.nvim",
+}
