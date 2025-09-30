b31e19b0dbf88bb8973f2dfb520fd998ea2f1338
Andrew Lazenka <andrewlazenka@gmail.com>
2025-09-30 10:03:44 -0400

    Move plugin configs

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -1,6 +1,12 @@
 return {
 	{
-	  'stevearc/oil.nvim',
-	  dependencies = { "nvim-tree/nvim-web-devicons" },
+		'stevearc/oil.nvim',
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		opts = {
+
+			view_options = {
+				show_hidden = true,
+			},
+		}
 	}
 }
