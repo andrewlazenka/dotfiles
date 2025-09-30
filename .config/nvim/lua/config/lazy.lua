ea733f136902d15c11b80158d315957b95bccd14
Andrew Lazenka <andrewlazenka@gmail.com>
2025-09-30 10:03:44 -0400

    Move plugin configs

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -12,3 +12,16 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup("plugins")
+
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    -- This command loads the plugin and sets the theme
+    -- It uses a function that ensures the plugin is loaded first.
+    -- The `load` function is specific to kanagawa.nvim
+    -- Alternatively, you can use `vim.cmd.colorscheme "kanagawa-wave"`
+    -- require('kanagawa').load("wave")
+	require('tokyonight').load("day")
+    -- Or if you prefer the generic way after ensuring it's loaded:
+    -- vim.cmd "colorscheme kanagawa-wave"
+  end,
+})
--- .config/nvim/lua/plugins/nvim-navic.lua
+++ .config/nvim/lua/plugins/nvim-navic.lua
@@ -1,6 +1,9 @@
 return {
 	{
 		"SmiteshP/nvim-navic",
-		dependencies = "neovim/nvim-lspconfig"
+		dependencies = "neovim/nvim-lspconfig",
+		config = function()
+			vim.o.winbar = "%{%v:lua.require'nvim-navic'.get_location()%}"
+		end,
 	}
 }
