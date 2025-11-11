01632b264f39fcb4821cd0d9c8c3ab968ecbbbc4
Andrew Lazenka <andrewlazenka@gmail.com>
2025-11-11 17:49:59 -0500

    keymaps

--- .config/nvim/lua/config/keymap.lua
+++ .config/nvim/lua/config/keymap.lua
@@ -1,10 +1,7 @@
--- nvim-tree
-vim.keymap.set("n", "<C-t>", ":NvimTreeToggle<CR>")
-vim.keymap.set("n", "<C-f>", ":Oil<CR>")
-
--- core
-vim.keymap.set("n", "<C-j>", "<PageDown>")
-vim.keymap.set("n", "<C-k>", "<PageUp>")
+vim.keymap.set("n", "<C-f>", ":Oil<CR>", { desc = 'Open file browser' })
+vim.keymap.set("n", "<C-j>", "<PageDown>", { desc = 'Scroll down a page' })
+vim.keymap.set("n", "<C-k>", "<PageUp>", { desc = 'Scroll up a page' })
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>', { desc = 'Clear search highlights' })
 
 -- quickfix
 -- vim.keymap.set("n", "<leader>qo", ":copen<CR>")
@@ -12,6 +9,15 @@ vim.keymap.set("n", "<C-k>", "<PageUp>")
 -- vim.keymap.set("n", "<leader>qn", ":cnext<CR>")
 -- vim.keymap.set("n", "<leader>qp", ":cprev<CR>")
 
--- move line under cursor (visual mode)
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = 'Move selected lines down' })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = 'Move selected lines up' })
+
+-- Make split navigation easier.
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+vim.keymap.set('n', '<leader>dio', function()
+  vim.diagnostic.setqflist({ open = true })
+end, { desc = 'Send all diagnostics to quickfix and open' })
