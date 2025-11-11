f670cc3aefc33d3ccce2b4899a371ca327bd80de
Andrew Lazenka <andrewlazenka@gmail.com>
2025-11-11 17:49:52 -0500

    telescope

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -1,18 +1,34 @@
 local telescope = require("telescope")
 local builtin = require("telescope.builtin")
 
-vim.keymap.set("n", "<leader>sf", builtin.find_files, {})
-vim.keymap.set("n", "<C-p>", builtin.git_files, {})
-vim.keymap.set("n", "<leader>cb", builtin.current_buffer_fuzzy_find)
-vim.keymap.set("n", "<leader>sb", builtin.buffers)
-vim.keymap.set("n", "<leader>sf", builtin.find_files)
-vim.keymap.set("n", "<leader>sh", builtin.help_tags)
-vim.keymap.set("n", "<leader>sw", builtin.grep_string)
-vim.keymap.set("n", "<leader>sg", builtin.live_grep)
-vim.keymap.set("n", "<leader>sd", builtin.diagnostics)
-vim.keymap.set("n", "<leader>smap", builtin.keymaps)
+vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = 'Find files' })
+vim.keymap.set("n", "<C-p>", builtin.git_files, { desc = 'Find git files' })
+vim.keymap.set("n", "<leader>cb", builtin.current_buffer_fuzzy_find, { desc = 'Fuzzy find in current buffer' })
+vim.keymap.set("n", "<leader>sb", builtin.buffers, { desc = 'List buffers' })
+vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = 'Find files' })
+vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = 'Search help tags' })
+vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = 'Grep string under cursor' })
+vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = 'Live grep' })
+vim.keymap.set("n", "<leader>st", builtin.lsp_type_definitions, { desc = 'Search type definition' })
+vim.keymap.set("n", "<leader>sdg", builtin.diagnostics, { desc = 'Show diagnostics' })
+vim.keymap.set("n", "<leader>sdb", function()
+	builtin.diagnostics({ bufnr = 0 })
+end, { desc = 'Show diagnostics for current buffer' })
+vim.keymap.set("n", "<leader>sm", builtin.keymaps, { desc = 'Show keymaps' })
 
 telescope.setup {
+	defaults = {
+		layout_strategy = 'horizontal',
+		layout_config = {
+			horizontal = {
+				width = {padding = 0},
+				height = {padding = 0},
+				prompt_position = "bottom",
+				preview_width = 0.5,
+				mirror = false,
+			},
+		},
+	},
 	pickers = {
 		find_files = {
 			hidden = true
