5a0d70e4ddc64805c91aef14d09d3e146c705208
Andrew Lazenka <andrewlazenka@gmail.com>
2026-02-02 16:16:14 -0500

    tmux theme link

--- setup/tmux.sh
+++ setup/tmux.sh
@@ -4,6 +4,10 @@
 mkdir -p ~/.config/tmux
 ln -s ~/code/andrewlazenka/dotfiles/.config/tmux/tmux.conf ~/.config/tmux/tmux.conf
 
+# link theme
+mkdir -p ~/.config/tmux/themes
+ln -s ~/code/andrewlazenka/dotfiles/.config/tmux/themes/tokyonight.conf ~/.config/tmux/themes/tokyonight.conf
+
 # install tpm for plugins
 mkdir -p ~/.tmux/plugins
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
