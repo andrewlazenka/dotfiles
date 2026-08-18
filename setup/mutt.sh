6c409a1c4c1b553a6539fcbb64078d8218ca030d
Andrew Lazenka <andrewlazenka@gmail.com>
2026-08-17 22:13:26 -0400

    remove neomutt

--- setup/mutt.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# move config into home directory
-
-mkdir -p ~/.config/neomutt
-rm ~/.config/neomutt/neomuttrc
-ln -s ~/Code/andrewlazenka/dotfiles/.config/neomutt ~/.config/neomutt
