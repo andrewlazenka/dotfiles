45357b762694e3ded1c8f381ef7e0370950767a2
Andrew Lazenka <andrewlazenka@gmail.com>
2026-08-21 09:10:55 -0400

    get rid of oh my zsh

--- setup/zsh.sh
+++ setup/zsh.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/env bash
 
-# Install Oh-My-Zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
 # move zsh profile into home directory
-rm ~/.zshrc
+rm -f ~/.zshrc
 ln -s ~/Code/andrewlazenka/dotfiles/.zshrc ~/.zshrc
 
 # change default shell
-chsh -s $(which zsh)
+chsh -s "$(which zsh)"
